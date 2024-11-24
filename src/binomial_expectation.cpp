6376daefc34326c4af47405eb07ff9a579cf3643
arunmalhi <arunmalhi8@gmail.com>
2024-11-24 19:15:54 +0000

    Submit Assignment

--- src/binomial_expectation.cpp
+++ src/binomial_expectation.cpp
@@ -2,21 +2,36 @@
 #include <vector>
 
 double binomial_expectation_loop(double *spread, double p, unsigned int n) {
-    std::vector<double> arr(n + 1, 0.0);
+    std::vector<double> arr(n + 1, 0.0);    // Create array using a vector of size n+1
 
     // set up the base nodes
-    // loop through each depth
-    // loop through each node at depth i
+    for (unsigned int i = 0; i<n+1; ++i){
+        arr[i] = spread[i];                 // For each element in the array, it is the spread (V(n,j) = spread(j))
+    }
 
-    double answer = arr[0];
+    // loop through each depth
+    for (int i = (int)n - 1; i >= 0; --i)
+    {
+        // loop through each node at depth i
+        for (unsigned int j = 0; j < i + 1; ++j)
+        {
+            arr[j] = (1.0 - p) * arr[j] + p * arr[j + 1];   // V(i,j) = (1-p)V(i+1,j) + pV(i+1,j+1)
+        }
+    }
+
+    double answer = arr[0];         //V(0,0)
     return answer;
 }
 
 double binomial_expectation_recursion_h(
     unsigned int i, unsigned int j, double *spread, double p, unsigned int n) {
-    // if (i == n)
-    // if (i < n)
-    return 0.0;
+    if (i == n)
+        return spread[j];
+
+    if (i < n)
+    // V
+    return (1.0 - p) * binomial_expectation_recursion_h(i+1,j,spread,p,n) +
+        p * binomial_expectation_recursion_h(i+1,j+1,spread,p,n);           //V(i,j) = (1-p)V(i+1,j) + pV(i+1,j+1)
 }
 
 double binomial_expectation_recursion(double *spread, double p, unsigned int n) {
@@ -63,4 +78,10 @@ void expected_returns(double S0,
     double d = 1.0 / u;
 
     // populate the output arrays
+    for (size_t i = 0; i<spread_points; ++i){
+        out_spread[i] = d + (u-d) * (double)i / ((double) spread_points-1.0);
+        out_risk_neutral_rate[i] = std::log(out_spread[i])/delta_t;
+        out_probability[i] =(out_spread[i]-d)/(u-d);
+        out_returns[i] = S0*expected_returns(S0,number_of_periods,volatility,time_period,out_spread[i]);
+    }
 }
